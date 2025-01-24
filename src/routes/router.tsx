2d4c01cdf2bcf57a423bf99616110a028495b17b
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-24 16:56:23 +0600

    Absolute path, relative path, outlet, and Layout layer.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,48 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Login from "../Pages/Login";
+import Registration from "../Pages/Registration";
+import AdminDashboard from "../Pages/Admin/AdminDashboard";
+import CreateStudent from "../Pages/Admin/CreateStudent";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<App/>
-    }
+        element:<App/>,
+        children:[
+            {
+                path:'about',
+                element:<About/>
+            },
+            {
+                path:'contact',
+                element:<Contact/>
+            },
+        ]
+    },
+    {
+        path:'/admin',
+        element:<App/>,
+        children:[
+            {
+                index:true,
+                element:<AdminDashboard/>
+            },
+            {
+                path:'create-student',
+                element:<CreateStudent/>
+            },
+        ]
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'/register',
+        element:<Registration/>
+    },
 ])
 export default router;
