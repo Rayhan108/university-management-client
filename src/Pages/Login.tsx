46408a1b2d130a809fac1d4c23718c4b6e0c4cae
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-27 23:27:05 +0600

    Login redirect and logout

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,25 +1,37 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { verifyToken } from "../utils/verifyToken";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
+    const navigate = useNavigate();
     const {register,handleSubmit}= useForm();
  const dispatch = useAppDispatch();
-    const [login,{error}]=useLoginMutation();
+    const [login]=useLoginMutation();
 
     
-    const onSubmit =async (data)=>{
-        const userInfo ={
-            id:data.id,
-            password:data.password
-        }
-     const res = await login(userInfo).unwrap();
-     const user = verifyToken(res.data.accessToken)
-     console.log("res=>",res);
-     dispatch(setUser({user:user,token:res.data.accessToken}))
+    const onSubmit =async (data:FieldValues)=>{
+  const toastId=toast.loading("Loggin in processes")
+  try{
+    const userInfo ={
+        id:data.id,
+        password:data.password
+    }
+ const res = await login(userInfo).unwrap();
+ const user = verifyToken(res.data.accessToken) as TUser
+ console.log("res=>",res);
+ dispatch(setUser({user:user,token:res.data.accessToken}))
+ toast.success("Logged in",{id:toastId,duration:2000})
+ navigate(`/${user.role}/dashboard`)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }catch(error){
+   
+    toast.error("something went wrong",{id:toastId,duration:2000})
+  }
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
