902eb291c910e4cd25180aa862cb7c25ad85ff4f
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-30 14:16:30 +0600

    fix typescript error

--- src/Pages/Admin/AademicManagement/AcademicSemister.tsx
+++ src/Pages/Admin/AademicManagement/AcademicSemister.tsx
@@ -2,83 +2,110 @@ import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemistersQuery } from "../../../redux/features/admin/academicManagement.api";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { useState } from "react";
+import { TQueryParam } from "../../../types";
 
 export type TTableData = Pick<
   TAcademicSemester,
-  'name' | 'year' | 'startMonth' | 'endMonth'
+  "name" | "year" | "startMonth" | "endMonth"
 >;
 
 const AcademicSemister = () => {
-  const [params,setParams]=useState([]);
-    const {data:semisterData}=useGetAllSemistersQuery(params);
-    
-    const tableData = semisterData?.data?.map(
-      ({ _id, name, startMonth, endMonth, year }) => ({
-        key: _id,
-        name,
-        startMonth,
-        endMonth,
-        year,
-      })
-    );
+  const [params, setParams] = useState <TQueryParam[]|undefined> (undefined);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { data: semisterData ,isLoading,isFetching} = useGetAllSemistersQuery(params);
 
-    const columns: TableColumnsType<TTableData> = [
-      {
-        title: 'Name',
-        key:'name',
-        dataIndex: 'name',
-        showSorterTooltip: { target: 'full-header' },
-        filters: [
-          {
-            text: 'Autumn',
-            value: 'Autumn',
-          },
-          {
-            text: 'Summar',
-            value: 'Summar',
-          },
-          {
-            text: 'Fall',
-            value: 'Fall',
-          },
-         
-        ],
-   
-      },
-      {
-        title: 'Year',
-        key:'year',
-        dataIndex: 'year',
-        
-      },
-      {
-        title: 'Start Month',
-        key:'startMonth',
-        dataIndex: 'startMonth',
-      },
-      {
-        title: 'End Month',
-        key:'endMonth',
-        dataIndex: 'endMonth',
-      },
-    ];
+  const tableData = semisterData?.data?.map(
+    ({ _id, name, startMonth, endMonth, year }) => ({
+      key: _id,
+      name,
+      startMonth,
+      endMonth,
+      year,
+    })
+  );
 
-    const onChange: TableProps<TTableData>['onChange'] = (pagination, filters, sorter, extra) => {
-      if(extra.action==='filter'){
-        const queryParams = []
-        filters.name?.forEach((item)=>(  queryParams.push({name:"name",value:item})))
-setParams(queryParams)
-      }
-    };
-        
-    return (
-      <Table<TTableData>
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+      filters: [
+        {
+          text: "Autumn",
+          value: "Autumn",
+        },
+        {
+          text: "Summar",
+          value: "Summar",
+        },
+        {
+          text: "Fall",
+          value: "Fall",
+        },
+      ],
+    },
+    {
+      title: "Year",
+      key: "year",
+      dataIndex: "year",
+      filters: [
+        {
+          text: "2024",
+          value: "2024",
+        },
+        {
+          text: "2025",
+          value: "2025",
+        },
+        {
+          text: "2026",
+          value: "2026",
+        },
+      ],
+    },
+
+    {
+      title: "Start Month",
+      key: "startMonth",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      key: "endMonth",
+      dataIndex: "endMonth",
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams : TQueryParam[] = [];
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: "year", value: item })
+      );
+      setParams(queryParams);
+    }
+  };
+// if(isLoading){
+//   return <p>Loading...</p>
+// }
+  return (
+    <Table<TTableData>
+      loading={isFetching}
       columns={columns}
       dataSource={tableData}
       onChange={onChange}
-      showSorterTooltip={{ target: 'sorter-icon' }}
+      showSorterTooltip={{ target: "sorter-icon" }}
     />
-    );
+  );
 };
 
 export default AcademicSemister;
